1ae3f97209b28165cc1948174bf6c7df055016ac
Aditya Patil <kwiktechlabs@gmail.com>
2025-07-23 18:31:21 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div className="bg-stone-800 grid h-screen">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addNewTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  // function editTask(index) {}
+
+  function moveTaskUp(index) {
+    const updatedTasks = [...tasks];
+    if (index > 0) {
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="bg-gray-200 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+      {/*Title of ToDo */}
+      <div className="flex items-center  justify-center mt-7 gap-2">
+        {/* <img src={todo_icon} className='w-8' /> */}
+        <p className="text-2xl bg-stone-800 rounded-3xl p-1">📃</p>
+        <h1 className="text-3xl font-semibold">To-Do List</h1>
+      </div>
+      {/*Add Todo Input */}
+      <div className="flex mt-4 justify-center">
+        <input
+          type="text"
+          id=""
+          placeholder="Add your task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="mx-2 rounded-2xl" onClick={addNewTask}>
+          ➕
+        </button>
+      </div>
+      <div className="tasks"></div>
+      <ol className="mt-2">
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            className="flex justify-between bg-stone-800 text-white mt-2 p-2 rounded-xl"
+          >
+            <span>{task}</span>
+            <div>
+              {/* <button className="mx-1" onClick={() => editTask(index)}>
+                ✏️
+              </button> */}
+              <button className="mx-1" onClick={() => moveTaskUp(index)}>
+                ⬆️
+              </button>
+              <button className="mx-1" onClick={() => moveTaskDown(index)}>
+                ⬇️
+              </button>
+              <button className="mx-1" onClick={() => deleteTask(index)}>
+                🗑️
+              </button>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default Todo;
